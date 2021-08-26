bffc16d81a74c256869802f666283bd5386c3a17
sora <575860117@qq.com>
2021-08-26 21:11:52 +0800

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import logo from "./assets/images/logo.svg";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robots";
+import RobotsDiscount from "./components/RobotsDiscount";
 import styles from "./App.module.css";
 import ShoppingCart from "./components/ShoppingCart";
 import { isPropertySignature } from "typescript";
+import RobotDiscount from "./components/RobotsDiscount";
 
 interface Props {}
 
@@ -54,9 +56,13 @@ const App: React.FC<Props> = (props) => {
       {(!error || error !== "") && <div>网站出错:{error}</div>}
       {!loading ? (
         <div className={styles.robotList}>
-          {robotGallery.map((r) => (
-            <Robot id={r.id} email={r.email} name={r.name} />
-          ))}
+          {robotGallery.map((r, index) =>
+            index % 2 == 0 ? (
+              <RobotDiscount id={r.id} email={r.email} name={r.name} />
+            ) : (
+              <Robot id={r.id} email={r.email} name={r.name} />
+            )
+          )}
         </div>
       ) : (
         <h2>loading</h2>
--- src/components/Robots.tsx
+++ src/components/Robots.tsx
@@ -1,37 +1,27 @@
 import React, { useContext } from "react";
 import styles from "./Robot.module.css";
 import { appContext, appSetStateContext } from "../AppState";
+import { withAddToCart } from "./AddToCart";
 
-interface RobotPrps {
+export interface RobotProps {
   id: number;
   name: string;
   email: string;
+  addToCart: (id, name) => void;
 }
 
-const Robot: React.FC<RobotPrps> = ({ id, name, email }) => {
+const Robot: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
   const value = useContext(appContext);
-  const setState = useContext(appSetStateContext);
-  const addToCart = () => {
-    if (setState) {
-      setState((state) => {
-        return {
-          ...state,
-          shoppingCart: {
-            items: [...state.shoppingCart.items, { id, name }],
-          },
-        };
-      });
-    }
-  };
+
   return (
     <div className={styles.cardContainer}>
       <img alt="robot" src={`https://robohash.org/${id}`} />
       <h2>{name}</h2>
       <p>{email}</p>
       <p>作者：{value.username}</p>
-      <button onClick={addToCart}>加入购物车</button>
+      <button onClick={() => addToCart(id, name)}>加入购物车</button>
     </div>
   );
 };
 
-export default Robot;
+export default withAddToCart(Robot);

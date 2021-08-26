bef515bbb4758000a5781c55f90af1cbcfe82146
sora <575860117@qq.com>
2021-08-26 18:14:42 +0800

    add_shopping_cart_function

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-const defaultContextValue = {
-  username: "sora",
-};
-
-export const appContext = React.createContext(defaultContextValue);
+import { AppStateProvider } from "./AppState";
 
 ReactDOM.render(
   <React.StrictMode>
-    <appContext.Provider value={defaultContextValue}>
+    <AppStateProvider>
       <App />
-    </appContext.Provider>
+    </AppStateProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
